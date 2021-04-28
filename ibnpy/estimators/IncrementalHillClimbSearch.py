6d5b99d42ca3b93c8e05a4ca302e2da77c6285b5
LuizAntonioPS <luiz.antonioPS95@gmail.com>
2021-04-28 15:58:29 -0300

    first commit

---
+++ ibnpy/estimators/IncrementalHillClimbSearch.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 28 15:57:18 2021
+
+@author: LuizPereira
+"""
+
---
+++ ibnpy/estimators/STEstimator.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 28 15:58:02 2021
+
+@author: LuizPereira
+"""
+
